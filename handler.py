c503472be46a39a29e1aef6ac44eb06927452d46
LuizGMMoschetta <luizzztavo@gmail.com>
2023-10-23 00:12:02 -0300

    corrigindo arquivo handler.py

--- handler.py
+++ handler.py
@@ -40,5 +40,5 @@ def rossmann_predict():
     
 if __name__ == '__main__':
 
-    port = os.environ.ger('PORT',5000)
+    port = os.environ.get('PORT',5000)
     app.run( host='0.0.0.0', port = port )
